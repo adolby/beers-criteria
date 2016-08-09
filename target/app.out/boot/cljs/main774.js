b15446bcb46dd77769933954d956ee3078f1c6e4
Andrew Dolby <andrewdolby@gmail.com>
2016-08-08 22:12:26 -0400

    Remove target from master branch

--- target/app.out/boot/cljs/main774.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main774');
-goog.require('cljs.core');
-goog.require('app.app');
-app.app.init();
--- target/app.out/reagent/debug.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {:static-fns true, :optimize-constants true}
-goog.provide('reagent.debug');
-goog.require('cljs.core');
